b5b1b208ac0c558a19fa36a3045a08f3cdf28eb7
AutumnAnson <aanson1@terpmail.umd.edu>
2024-12-01 20:20:48 -0500

    Lecture 3 links

--- src/Pages/Lecture.tsx
+++ src/Pages/Lecture.tsx
@@ -45,7 +45,33 @@ export default function Lecture(){
                     </a>
                 </li>
          </ul>
-     </div>
+        </div>
+
+
+        <div className="lecture-section">
+            <h2>Lecture 3</h2>
+            <ul>
+                <li>
+                    <a
+                        href="https://docs.google.com/presentation/d/1RWvO8TQ_ueJyBdSHfZ6oNvq9E6J3rYovItbX_Q-r-44/edit?usp=sharing"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Lecture Slides
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://umd.zoom.us/rec/share/SfirrOZAemFf_xSm3zhZq7j_3G9vTJfKWoXr4YIJD7V3pxjMpQe_AIjGT-Wk3Tdv.R_nKA8KwWqv8umwS?startTime=1728428448000"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Lecture Video
+                    </a>
+                </li>
+         </ul>
+        </div>
+
     </div>
     );
 }
