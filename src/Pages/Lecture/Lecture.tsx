c80a04c83d14671930824ddefeda6dedabec016e
AutumnAnson <aanson1@terpmail.umd.edu>
2024-12-02 12:02:57 -0500

    Rearranged lecture

--- src/Pages/Lecture/Lecture.tsx
+++ src/Pages/Lecture/Lecture.tsx
@@ -1,6 +1,7 @@
+import './Lecture.css'
 export default function Lecture(){
     return (
-        <div className='lecture-div'>
+    <div className='lecture-div'>
             <h1>LECTURE</h1>
             <p>On this page you will find lecture recordings and slides.</p>
         <div className="lecture-section">
